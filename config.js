def6295416bc9c534efec524247947f662592d2a
UNKNOWN-SAINT <nifemioluwa154@gmail.com>
2025-01-14 12:05:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require('dotenv').config(); 
 
 global.owner = process.env.OWNER_NUMBER.split(','); 
-global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
+global.anitav4 = process.env.BOT_NAME || "⚜️HOLY_SAINT-𝐕𝟒⚜️";
+global.ownername = process.env.OWNER_NAME || "⚜️⚜️ SAINT ⚜️⚜️";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
 global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
